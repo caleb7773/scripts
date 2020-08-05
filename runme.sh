eca7544271bf55da709b5bd6a3c450bba1be961e
caleb7773 <37095625+caleb7773@users.noreply.github.com>
2020-08-05 16:13:36 -0400

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+
+#cd scripts && sudo chmod +x ./runme.sh && ./runme.sh && ./openvpnP2P_server_install.sh
+#cd scripts && sudo chmod +x ./runme.sh && ./runme.sh && ./openvpnP2P_client_install.sh
+#cd scripts && sudo chmod +x ./runme.sh && ./runme.sh && ./startup.sh
+#cd scripts && sudo chmod +x ./runme.sh && ./runme.sh && ./startup_quiet.sh
+#cd scripts && sudo chmod +x ./runme.sh && ./runme.sh && ./build_client.sh
+#cd scripts && sudo chmod +x ./runme.sh && ./runme.sh && ./build_server.sh
+
 sudo chmod +x -R ../scripts
